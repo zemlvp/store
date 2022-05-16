76b92df4e9240d278dd9ca1e2cdb06a9e1c76309
zemlvp <zemlvp@yandex.ru>
2022-05-16 23:10:28 +0300

    Описать сущность Book

---
+++ domain/Store/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Store;
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; }
+    public Book(int id, string title)
+    {
+        Id = id;
+        Title = title;
+    }
+}
